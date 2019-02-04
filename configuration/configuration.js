59f24b0abee4ef028a5b97e15e8c3363f9d1b8da
Dr. Safi <sfmissive@gmail.com>
2019-02-04 17:34:50 +0200

    ⚗ Refactor dev dependencies and script configurations.

--- configuration/configuration.js
+++ configuration/configuration.js
@@ -1,15 +1,9 @@
 const path = require('path')
+const { script } = require('./script.config.js')
 
 module.exports = {
     directory: {
-        application: {
-            rootPath: path.resolve(`${__dirname}/..`),
-        },
+        root: path.resolve(`${__dirname}/..`),
     },
-    script: [
-        {
-            type: 'directory',
-            path: './script/'
-        }
-    ]
+    script
 }
