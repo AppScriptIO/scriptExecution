cb917859eba9923fda3ed55b3ee73bdeea2f349b
Safi <sfmissive@gmail.com>
2019-05-07 21:28:59 +0300

    ⚗ Refactor to allow manual building and releasing of package.

--- source/entrypoint/module/transpilation.entrypoint.js
+++
@@ -1,9 +0,0 @@
-// const transpileMacros = require('@dependency/transpileMacros.js')
-// transpileMacros('babel-plugin-...') // executes babel register with specified babel plugins.
-
-/* Entrypoint chain */
-// • Transpilation (babelJSCompiler)
-require('@dependency/javascriptTranspilation')({ babelConfigurationFile: 'serverRuntime.BabelConfig.js' })
-
-// • Run
-module.exports = require('./')
