24f74a9f3032892cd19d1bf408ebe3a21e96dce5
Safi <nassarsafi@gmail.com>
2020-01-19 23:42:37 +0200

    ✒ Add options for terminating vm context on process exit.

--- source/executionAlgorithm/executeOnCall.js
+++ source/executionAlgorithm/executeOnCall.js
@@ -1,42 +1,41 @@
 import path from 'path'
-import assert from 'assert' 
+import assert from 'assert'
 import util from 'util'
 const style = { titleCyan: '\x1b[33m\x1b[1m\x1b[7m\x1b[36m', titleGolden: '\x1b[33m\x1b[1m\x1b[7m', message: '\x1b[96m', italic: '\x1b[2m\x1b[3m', default: '\x1b[0m' }
 
 /**
  * Execute `scriptCofnig.type == 'module'` i.e. value is exported.
  */
-export function executeOnCall({ 
-    scriptPath, 
-    methodName, 
-    parameter = [], 
-    adapterFunction = null // the adapter must return a function where it encapsulates the specific needed implementation for the script.
+export function executeOnCall({
+  scriptPath,
+  methodName,
+  parameter = [],
+  adapterFunction = null, // the adapter must return a function where it encapsulates the specific needed implementation for the script.
 }) {
-    assert(path.isAbsolute(scriptPath), `• 'scriptPath' must be an absolute path to be executed.`)
-    console.log(`${style.italic}${style.titleGolden}%s${style.default} - %s`, `•[JS module]`, `Running ${scriptPath}`)
-    console.log(`\t\x1b[2m\x1b[3m%s\x1b[0m \x1b[95m%s\x1b[0m`, `File path:`, `${scriptPath}`)
-    let requiredModule = require(scriptPath)
-    let func; // function to execute
+  assert(path.isAbsolute(scriptPath), `• 'scriptPath' must be an absolute path to be executed.`)
+  console.log(`${style.italic}${style.titleGolden}%s${style.default} - %s`, `•[JS module]`, `Running ${scriptPath}`)
+  console.log(`\t\x1b[2m\x1b[3m%s\x1b[0m \x1b[95m%s\x1b[0m`, `File path:`, `${scriptPath}`)
+  let requiredModule = require(scriptPath)
+  let func // function to execute
 
-    switch (typeof requiredModule) {
-        case 'object':
-            assert(typeof requiredModule[methodName] == 'function', `• "${methodName}" must match property of required module and be a function: ${util.inspect(requiredModule)}`)
-            func = requiredModule[methodName]
-        break;
-        case 'function': 
-            func = requiredModule
-        break; 
-        default:
-            let partialMessage =  (methodName) ? 'object' : 'function';
-            throw new Error(`• Required script ${scriptPath} must be a ${partialMessage}, i.e. the script is expected to export a ${partialMessage}. Required script: ${util.inspect(requiredModule)}`)
-        break;
-    }
+  switch (typeof requiredModule) {
+    case 'object':
+      assert(typeof requiredModule[methodName] == 'function', `• "${methodName}" must match property of required module and be a function: ${util.inspect(requiredModule)}`)
+      func = requiredModule[methodName]
+      break
+    case 'function':
+      func = requiredModule
+      break
+    default:
+      let partialMessage = methodName ? 'object' : 'function'
+      throw new Error(`• Required script ${scriptPath} must be a ${partialMessage}, i.e. the script is expected to export a ${partialMessage}. Required script: ${util.inspect(requiredModule)}`)
+      break
+  }
 
-    if(!Array.isArray(parameter)) parameter = [parameter]
+  if (!Array.isArray(parameter)) parameter = [parameter]
 
-    if(adapterFunction) {
-        func = adapterFunction({ callback: func, args: parameter})
-        func()
-    } else
-        func(...parameter) // execute function from module passing it the parameters.
+  if (adapterFunction) {
+    func = adapterFunction({ callback: func, args: parameter })
+    func()
+  } else func(...parameter) // execute function from module passing it the parameters.
 }
--- source/executionAlgorithm/executeOnRequire.js
+++ source/executionAlgorithm/executeOnRequire.js
@@ -1,25 +1,24 @@
 import path from 'path'
 const style = { titleCyan: '\x1b[33m\x1b[1m\x1b[7m\x1b[36m', titleGolden: '\x1b[33m\x1b[1m\x1b[7m', message: '\x1b[96m', italic: '\x1b[2m\x1b[3m', default: '\x1b[0m' }
-import assert from 'assert' 
+import assert from 'assert'
 
 /**
  * Execute `scriptCofnig.type == 'script'`
  * Usage:
- *  # Extracting the parameters from the script: `const [parameter1, parameter2] = process.argv.parameter` 
+ *  # Extracting the parameters from the script: `const [parameter1, parameter2] = process.argv.parameter`
  *    e.g. `const [api] = process.argv.parameter` or `const [{ project: projectApi }] = process.argv.parameter`
  */
-export function executeOnRequire({ 
-    scriptPath, 
-    parameter = null // parameters are passed using an environment variable. 
+export function executeOnRequire({
+  scriptPath,
+  parameter = null, // parameters are passed using an environment variable.
 }) {
-    assert(path.isAbsolute(scriptPath), `• 'scriptPath' must be an absolute path to be executed.`)
-    console.log(`${style.italic}${style.titleGolden}%s${style.default} - %s`, `•[JS script]`, `Running ${scriptPath}`)
-    console.log(`\t\x1b[2m\x1b[3m%s\x1b[0m \x1b[95m%s\x1b[0m`, `File path:`, `${scriptPath}`)
+  assert(path.isAbsolute(scriptPath), `• 'scriptPath' must be an absolute path to be executed.`)
+  console.log(`${style.italic}${style.titleGolden}%s${style.default} - %s`, `•[JS script]`, `Running ${scriptPath}`)
+  console.log(`\t\x1b[2m\x1b[3m%s\x1b[0m \x1b[95m%s\x1b[0m`, `File path:`, `${scriptPath}`)
 
-    // pass parameter to script 
-    // another option for passing parameter could be `process.argv.push(...parameter)`
-    process.argv['parameter']= parameter // attach to the global `process.argv` object, this will not affect the argv array yet allow access to the parameters
+  // pass parameter to script
+  // another option for passing parameter could be `process.argv.push(...parameter)`
+  process.argv['parameter'] = parameter // attach to the global `process.argv` object, this will not affect the argv array yet allow access to the parameters
 
-    require(scriptPath)
+  require(scriptPath)
 }
-
